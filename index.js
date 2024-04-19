bdca708edcb2d194babade734113da8954c858ec
N-Soni0 <anton.stecyura.pr1@gmail.com>
2024-04-19 15:08:53 +0300

    App init

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!')
+import express from 'express';
+const app = express()
+const port = 3000
+import users from  './db.json' assert { type: 'json'};
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/users', async(req, res) => {
+    
+    res.json(users)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
